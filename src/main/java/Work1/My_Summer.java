2c4363d07e13350452eacec4555fbbcebe934a15
Vandini <egor96471@gmail.com>
2019-11-23 21:34:06 +0200

    complite work

---
+++ src/main/java/Work1/My_Summer.java
@@ -0,0 +1,7 @@
+package Work1;
+
+public class My_Summer {
+    public static void main(String[] args) {
+        System.out.println("\t\tТема: як я провів літо.\n На протязі всього літа я працював в фірмі MindySupport на проекті Pixeline на посаді графічного редактора. ");
+    }
+}
