14cedb66c17113cfa295163609bf68b564885d1e
Vandini <egor96471@gmail.com>
2019-11-24 01:30:50 +0200

    Matematica Complete

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,8 @@ package Work1;
 public class Test {
     public static void main(String[] args) {
 
-
+float x=1;
+        System.out.println(x);
 
     }
 }
--- src/main/java/Work1/Matematica.java
+++ src/main/java/Work1/Matematica.java
@@ -1,4 +1,24 @@
 package Work1;
 
+import java.util.Scanner;
+
 public class Matematica {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\tВітаю\nВведіть 2 числа:\nПерше число: ");
+        Float x = scanner.nextFloat();
+        System.out.println("Друге число:");
+        Float y = scanner.nextFloat();
+
+        System.out.println(x+y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x-y);
+        System.out.println(x%y);
+        System.out.println(x.equals(y));
+        System.out.println(x<y);
+        System.out.println(x>y);
+
+    }
 }
