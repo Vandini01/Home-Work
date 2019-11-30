f7fe51e6a632e6948a41af1bfec6841671ac7d23
Vandini <egor96471@gmail.com>
2019-11-30 21:59:39 +0200

    work2 complete

---
+++ src/main/java/work2/Exercise3.java
@@ -0,0 +1,11 @@
+package work2;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Exercise2Method method = new Exercise2Method();
+        int x = method.inputWidth();
+        int y = method.inputHeight();
+        method.drawRectangle(x,y);
+
+    }
+}
